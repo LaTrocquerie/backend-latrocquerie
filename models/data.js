e99ca8db65e68bbfdf63c214d0316fdb90e5bea9
Victor Pagnier <victor.pagnier@gmail.com>
2022-01-13 17:28:24 +0100

    La première route get fonctionne

--- models/data.js
+++ models/data.js
@@ -2,18 +2,9 @@ const connection = require('../db-config');
 
 const db = connection.promise();
 
-/**
- * Methode pour retrouver un film via son id
- * @param {*} id (req.params)
- * @returns
- */
-const findOne = (id_pages) => db
-  .query('SELECT * FROM pages WHERE id_pages = ?', [id_pages])
-  .then((result) => {
-    console.log(result[0]);
-    console.log(id_pages);
-    return result[0][0];
-  });
+const findOne = (id) => db
+  .query('SELECT * FROM pages WHERE id_pages = ?', [id])
+  .then((result) => result[0][0]);
 
 module.exports = {
   findOne,
