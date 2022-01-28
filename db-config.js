3fd1b582bb97edfd8899dd2647ec885b27d4c512
François Chatelier <francois.chatelier@gmail.com>
2022-01-28 13:16:05 +0100

    remise à niveau import DB

--- db-config.js
+++ db-config.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const connection = mysql.createPool(
-  "mysql://bde6686755a8a9:94c19bc4@eu-cdbr-west-02.cleardb.net/heroku_f4eff49e44b9067?reconnect=true"
-);
+const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 // Paramétrage de la connection avec le server (3 dernières lignes) pour éviter qu'il saute
 
