0d38da853657b5e69675fa8fe72bef267ef1f8cb
François Chatelier <francois.chatelier@gmail.com>
2022-01-28 12:56:25 +0100

    test app

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
-const multer = require('multer');
-const connection = require('./db-config');
+const multer = require("multer");
+// const connection = require('./db-config');
 
-const { setupRoutes } = require('./routes');
+// const { setupRoutes } = require("./routes");
 
 const PORT = process.env.PORT || 8000;
 
 // paramétrage d'une nouvelle connexion (getConnection)
-connection.getConnection((err, conn) => {
-  if (err) console.log('Erreur de connexion à la DB', err);
-  else console.log(`Connexion à la DB ok, id${conn.threadId}`);
-});
+// connection.getConnection((err, conn) => {
+//   if (err) console.log("Erreur de connexion à la DB", err);
+//   else console.log(`Connexion à la DB ok, id${conn.threadId}`);
+// });
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
-setupRoutes(app);
+// setupRoutes(app);
+app.get("/", (req, res) => {
+  res.send("app qui tourne");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
@@ -28,10 +31,10 @@ app.listen(PORT, () => {
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, '../frontend_latrocquerie/public/assets/images');
+    callback(null, "../frontend_latrocquerie/public/assets/images");
   },
   filename: (req, file, callback) => {
-    const provisoireFileName = file.originalname.split('.');
+    const provisoireFileName = file.originalname.split(".");
     const date = Date.now();
     req.newName = `${provisoireFileName[0]}_${date}.${provisoireFileName[1]}`;
     callback(null, req.newName);
@@ -40,10 +43,10 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-app.get('/upload', (req, res) => {
+app.get("/upload", (req, res) => {
   res.json(req.body);
 });
 
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post("/upload", upload.single("file"), (req, res) => {
   res.json(`./assets/images/${req.newName}`);
 });
