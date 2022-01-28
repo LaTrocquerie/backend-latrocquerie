cb7922d2750c0e0d0d13fa28e4e5061554535ada
François Chatelier <francois.chatelier@gmail.com>
2022-01-28 15:08:36 +0100

    correction d'erreur sur la restructuration + validation

--- models/articleImage.js
+++ models/articleImage.js
@@ -1,22 +1,22 @@
-// const connection = require("../db-config");
+const connection = require("../db-config");
 
-// const db = connection.promise();
+const db = connection.promise();
 
-// const findArticleImage = async (id) => {
-//   try {
-//     const articleimage = await db.query(
-//       "SELECT * FROM articleimage WHERE id_pages = ?",
-//       [id]
-//     );
-//     return articleimage[0].map((articleImage) => ({
-//       component: "articleImage",
-//       data: articleImage,
-//     }));
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
+const findArticleImage = async (id) => {
+  try {
+    const articleimage = await db.query(
+      "SELECT * FROM articleimage WHERE id_pages = ?",
+      [id]
+    );
+    return articleimage[0].map((articleImage) => ({
+      component: "articleImage",
+      data: articleImage,
+    }));
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-// module.exports = {
-//   findArticleImage,
-// };
+module.exports = {
+  findArticleImage,
+};
