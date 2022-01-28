28bcbbc689f859420279f4731cd1786f60e3f1d6
François Chatelier <francois.chatelier@gmail.com>
2022-01-28 09:55:51 +0100

    Nettoyage de code et mise en place d'un fichier par model + gestion de conflits

--- routes/pages.js
+++ routes/pages.js
@@ -1,21 +1,16 @@
-<<<<<<< HEAD
 const routerPages = require("express").Router();
 const { findOne, createOne } = require("../models/pages");
-=======
-const routerPages = require('express').Router();
-const { findOne, createOne } = require('../models/pages');
->>>>>>> c6a0eaa8fb47125a203732932b94a826fef5acec
 
 // Route GET
 
-routerPages.get('/:page', async (req, res) => {
+routerPages.get("/:page", async (req, res) => {
   const result = await findOne(req.params.page);
   res.status(200).json(result);
 });
 
 // Route POST
 
-routerPages.post('/:page', async (req, res) => {
+routerPages.post("/:page", async (req, res) => {
   const result = await createOne(req.body);
   res.status(201).json(result);
 });
