35d26a9a673bf7bda853a12ada81d41d40339f5a
Victor Pagnier <victor.pagnier@gmail.com>
2022-01-31 12:10:59 +0100

    Refait les routes email & upload d'image

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const routerPages = require("./pages");
 const routerHeader = require("./header");
 const routerFooter = require("./footer");
+const routerImage = require("./uploadImage");
+const routerEmail = require("./email");
 
 const setupRoutes = (app) => {
   app.use("/api/pages", routerPages);
   app.use("/api/header", routerHeader);
   app.use("/api/footer", routerFooter);
+  app.use("/api/upload", routerImage);
+  app.use("/api/email", routerEmail);
 };
 
 module.exports = { setupRoutes };
