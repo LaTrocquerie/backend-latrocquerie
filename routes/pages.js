b275260c0df36cce06cf1d381a4fb2f6e71f8373
François Chatelier <francois.chatelier@gmail.com>
2022-01-31 14:41:32 +0100

    correction qui retourne sur 404

--- routes/pages.js
+++ routes/pages.js
@@ -1,18 +1,22 @@
-const routerPages = require('express').Router();
-const { findOne, createOne } = require('../models/pages');
+const routerPages = require("express").Router();
+const { findOne, createOne } = require("../models/pages");
 
 // Route GET
 
-routerPages.get('/:page', async (req, res) => {
+routerPages.get("/:page", async (req, res) => {
   const result = await findOne(req.params.page);
-  result.components.sort((a, b) => a.data.bloc_order - b.data.bloc_order);
-
-  res.status(200).json(result);
+  console.log(result);
+  if (result) {
+    result.components.sort((a, b) => a.data.bloc_order - b.data.bloc_order);
+    res.status(200).json(result);
+  } else {
+    res.status(404).send("Page introuvable");
+  }
 });
 
 // Route POST
 
-routerPages.post('/:page', async (req, res) => {
+routerPages.post("/:page", async (req, res) => {
   const result = await createOne(req.body);
   res.status(201).json(result);
 });
