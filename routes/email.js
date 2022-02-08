644b58c74b2bece23bccb431dc7de82756274e18
Victor Pagnier <victor.pagnier@gmail.com>
2022-02-08 12:21:11 +0100

    Le service de mail est maintenant fonctionnel et actif

--- routes/email.js
+++ routes/email.js
@@ -3,30 +3,45 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: 'stmp.gmail.com',
   auth: {
-    user: '',
-    pass: '',
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
   },
 });
 
-const mailOptions = {
-  from: 'La Trocquerie <latrocquerie@gmail.com>', // de qui?
-  to: 'latrocquerie@gmail.com', // pour qui?
-  subject: 'Votre demande a été transmise', // sujet
-  html: `
-  <h1 style="color:#9a1d1f;">La Trocquerie</h1>
-  <h2 style="color:black;">Bonjour,</h2>
-  <p style="color:black;">Votre message a bien été pris en compte.</p>
-  <img src="https://i.imgur.com/NpBR5Nm.jpg" alt="Logo La Trocquerie" height="150" width="auto" />
-  `, // message
-};
-
-routerEmail.get('/', (req, res) => {
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      console.log(err);
-    } else { console.log(info); }
-  });
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
 });
+
+routerEmail.post('/send', (req, res, next) => {
+  const email = req.body.email
+  const subject = req.body.subject
+  const name = req.body.name
+  const message = req.body.message
+
+  const mail = {
+    from: `latrocquerie@gmail.com`,
+    to: `latrocquerie@gmail.com`,
+    subject: `WEBSITE: ${subject}`,
+    text: `${email} ${name} ${message}`,
+  }
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail'
+      })
+    } else {
+      res.json({
+       status: 'success'
+      })
+    }
+  })
+})
 
 module.exports = routerEmail;
